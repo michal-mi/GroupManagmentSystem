fe26fd74d262e324a3b467b70fa712a7fc2d4e47
Michał Mielcarek <65447595+michal-mi@users.noreply.github.com>
2022-05-22 18:59:31 +0200

    Update v2

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({limit: "100mb", extended:true}));
 //app.use(cors());
 
 
-const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.fhmbr.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://admin:admin@cluster0.fhmbr.mongodb.net/?retryWrites=true&w=majorityy';
 
 const PORT = process.env.PORT || 5000;
 
