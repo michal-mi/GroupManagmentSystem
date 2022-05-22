d49cc3c89632f36ce65135803466afdadad384ca
Michał Mielcarek <65447595+michal-mi@users.noreply.github.com>
2022-05-22 19:08:48 +0200

    Update v3

---
+++ server/models/member.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const memberSchema = mongoose.Schema({
+    "ID": UUID(),
+	"groupID": UUID(),
+	"name": String,
+	"secondName": String,
+	"lastName": String,
+	"age": Int16,
+	"function": String,
+	"rank": String,
+	"phoneNumber": String,
+	"email": String,
+	"PESEL": Int16,
+	"dateOfJoining": Timestamp,
+	"address":{
+		"street": String,
+		"houseNumber": String,
+		"flatNumber": String,
+		"city": String,
+		"zipCode": String
+	},
+	"parent1":{
+		"name": String,
+		"lastName": String,
+		"phoneNumber": String,
+		"email": String
+	},
+	"parent2":{
+		"name": String,
+		"lastName": String,
+		"phoneNumber": String,
+		"email": String
+	}
+})
