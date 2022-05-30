4b0162358dddf221db77023832715788cac75035
Michał Mielcarek <65447595+michal-mi@users.noreply.github.com>
2022-05-30 18:44:41 +0200

    Update v7

--- client/src/components/showMember/showMember.js
+++ client/src/components/showMember/showMember.js
@@ -7,13 +7,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein){
   return { name, calories, fat, carbs, protein };
 }
 
