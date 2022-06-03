c27d80c1204add0ac070544f39bc32b3d753ed70
Michał Mielcarek <65447595+michal-mi@users.noreply.github.com>
2022-06-03 23:55:42 +0200

    Update v15

--- GroupManagmentSystemV2/client/src/components/editGroup/index.jsx
+++ GroupManagmentSystemV2/client/src/components/editGroup/index.jsx
@@ -1,33 +1,38 @@
 import styles from "./styles.module.css"
 import { Link, useNavigate } from "react-router-dom"
 import axios from "axios";
-import {React, useEffect, useState} from "react";
+import { React, useEffect, useState } from "react";
 
 export default function ShowGroup() {
 
+    const navigate = useNavigate()
+    const id = window.location.href.slice(-24)
     const [error, setError] = useState("")
     const [data, setData] = useState({
         name: "",
         dateOfCreation: "",
         description: ""
     })
-    const navigate = useNavigate()
     const [groupsList, setGroupList] = useState([])
-    const id = window.location.href.slice(-24)
-    var groupName = returnGroupName()
-    console.log(groupName)
-    const [groupName1, setGroupName1] = useState({})
-    var groupInfo = ""
+    const [groupName, setGroupName] = useState()
+    const [groupDate, setGroupDate] = useState()
+    const [groupDescription, setGroupDescription] = useState()
     var groupDelButton = ""
 
-    function xd(){
-        setGroupName1(groupName1=>[...groupName1, groupName])
+    function setEdition() {
+        setGroupName(returnGroupName())
+        setGroupDate(returnGroupDate().slice(0, 10))
+        setGroupDescription(returnGroupDescription())
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        data.name = groupName
+        data.dateOfCreation = groupDate
+        data.description = groupDescription
         try {
-            const url = "http://localhost:8080/api/groups"
+            axios.delete(`http://localhost:8080/api/groups/${id}`)
+            const url = `http://localhost:8080/api/groups/`
             const { data: res } = await axios.post(url, data)
             navigate("/groups")
         } catch (error) {
@@ -41,34 +46,34 @@ export default function ShowGroup() {
         }
     }
 
-    const handleChange = ({ currentTarget: input }) => {
-        setData({ ...data, [input.name]: input.value })
-    }
-
     function returnGroupName() {
+        var groupName
         groupsList.map((group, key) => {
             if (group._id === id) {
                 groupName = group.name
             }
         })
-        //setGroupName1(groupName.target.value)
         return groupName
     }
 
-    function returnGroupInfo() {
+    function returnGroupDate() {
+        var groupDate
         groupsList.map((group, key) => {
             if (group._id === id) {
-                groupInfo = <div>
-                    <h2><b>Nazwa grupy: </b>{group.name}</h2>
-                    <div className={styles.center}><b>Data utworzenia grupy:</b></div>
-                    <div className={styles.center}>{group.dateOfCreation.slice(0, 10)}</div>
-                    <br></br>
-                    <div className={styles.center}><b>Opis grupy:</b></div>
-                    <div className={styles.center}>{group.description}</div>
-                </div>
+                groupDate = group.dateOfCreation
             }
         })
-        return groupInfo
+        return groupDate
+    }
+
+    function returnGroupDescription() {
+        var groupDescription
+        groupsList.map((group, key) => {
+            if (group._id === id) {
+                groupDescription = group.description
+            }
+        })
+        return groupDescription
     }
 
     function returnGroupDelButton() {
@@ -76,7 +81,7 @@ export default function ShowGroup() {
             if (group._id === id) {
                 groupDelButton = <button
                     className={styles.delete_btn} onClick={() => deleteGroup(group._id)}>
-                    Usuń
+                    Usuń grupę
                 </button>
             }
         })
@@ -86,10 +91,6 @@ export default function ShowGroup() {
     useEffect(() => {
         axios.get("http://localhost:8080/api/groups").then((allGroups) => {
             setGroupList(allGroups.data)
-            setGroupName1(groupName)
-            console.log(groupName)
-            //xd() pobierać dane z linku i dopiero zmodyfikowane zapisywać do bazy danych
-            console.log(groupName)
         })
     }, [])
 
@@ -101,49 +102,66 @@ export default function ShowGroup() {
     }
 
     return (
-        <div>
-        <div className={styles.center}><h2>Kreator grupy</h2></div>
-        <form className={styles.form_container} onSubmit={handleSubmit}>
-            <input
-                type="text"
-                placeholder="Nazwa grupy"
-                //name="groupName1" how to display and modify const in textfield react
-                //onChange={(e)=>setGroupName1(e.target.value)}
-                onChange={groupName1}
-                value={groupName1}
-                className={styles.input}
-            />
-            <p>
-                <div className={styles.special}>Data utworzenia:</div>
-                <input
-                    type="date"
-                    placeholder="Data utworzenia grupy"
-                    name="dateOfCreation"
-                    onChange={handleChange}
-                    value={data.dateOfCreation}
-                    className={styles.input1}
-                />
-            </p>
-            <input
-                type="text"
-                placeholder="Opis"
-                name="description"
-                onChange={handleChange}
-                value={data.description}
-                className={styles.input}
-            />
-            <div class={styles.center}>
-                {error && <div className={styles.error_msg}>{error}</div>}
-                <button
-                    type="submit"
-                    className={styles.green_btn}>
-                    Zapisz zmiany
+        <div className={styles.main_container}>
+
+            <nav className={styles.navbar}>
+                <h1>Edytowanie {returnGroupName()}</h1>
+                <Link to="/groups">
+                    <button
+                        className={styles.exit_btn}>
+                        Powrót
+                    </button>
+                </Link>
+            </nav>
+
+            <div className={styles.center}>
+                <button className={styles.start_edit_btn} onClick={() => setEdition()}>
+                    Potwierdź kontynuowanie edytowania!
                 </button>
             </div>
-        </form>
-        <div className={styles.center}>
-                {returnGroupDelButton()}
-            </div>
+
+            <div className={styles.center}><h2>Edytor grupy</h2></div>
+
+            <form className={styles.form_container} onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Nazwa grupy"
+                    name="groupName"
+                    onChange={(e) => setGroupName(e.target.value)}
+                    value={groupName}
+                    className={styles.input}
+                />
+                <p>
+                    <div className={styles.special}>Data utworzenia:</div>
+                    <input
+                        type="date"
+                        placeholder="Data utworzenia grupy"
+                        name="dateOfCreation"
+                        onChange={(e) => setGroupDate(e.target.value)}
+                        value={groupDate}
+                        className={styles.input1}
+                    />
+                </p>
+                <input
+                    type="text"
+                    placeholder="Opis"
+                    name="description"
+                    onChange={(e) => setGroupDescription(e.target.value)}
+                    value={groupDescription}
+                    className={styles.input}
+                />
+                <div className={styles.center}>
+                    {returnGroupDelButton()}
+                </div>
+                <div class={styles.center}>
+                    {error && <div className={styles.error_msg}>{error}</div>}
+                    <button
+                        type="submit"
+                        className={styles.green_btn}>
+                        Zapisz zmiany
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
